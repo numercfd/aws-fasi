85d3d259fbbf67005d902d8b46bcf53ee8df8295
Matheus Cansian <mscansian@gmail.com>
2016-01-18 15:31:56 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Failover AWS Spot Instances
+
+This lambda script allows your AWS account to use spot instances that automatically failover to on-demand instances on pricing spikes. It works by setting up two *auto scaling groups* and a *lambda function* that pools the groups and changes the *desired capacity* accordingly.
+
+Note: This will incur in additional AWS Lambda charges, but they are small compared to the savings of using spot instances.
+
+**Failover Delay:** 5 to 10 minutes
+
+**License:** MIT
+
+## Usage Scenarios
+### Very-low-cost LAMP server
+You could use this script to set-up a low-cost spot instance, such as *t1.micro*, as your LAMP server. If your instance is terminated during pricing spikes, it is automatically replaced by an on-demand instance. When prices drop, the on-demand instance terminated.
+
+### Persistent Spot Instances Cluster
+Imagine that you have to do lots of computation and need lots of instances. AWS provide you with two approaches: The first one is to use normal instances. Effective, but costly; The second option are spot instances. While they are cheap, if you are going through some pricing spike then you have no idea when the computation will be finished. Why not use a hybrid approach?
+You can set-up two auto scaling groups, one for the spot instances and other for on-demand. Only when your spot group is unable to launch instances, the on-demand group is used. This allows you to have the agility of on-demand instances, but also the pricing benefits of spot instances.
+
+## Lifecycle
+```
+----0------X-------------0---------I----------0-------
+  Pool  Failure      Failover  Launched  Elastic IP
+```
+
+1. Pool: Lambda function check instances
+2. Failure: A spot instance has failed
+3. Failover: Lambda function launches an on-demand replacement
+4. Launched: Replacement is running
+5. Elastic IP: Lambda function assigns the elastic ip
+
+## Configuring
+**NOTE: You only need to do this once per AWS account. A single function can handle multiple auto scaling groups.**
+
+Create a new Python 2.7 function in AWS Lambda and paste the code found in "lambda.py" file. Make sure the following configurations are set.
+
+Configuration|What to do?
+-----|------
+Function Runtime|Python 2.7
+Function Handler|fasi.main
+Function Role|(create a new role and add the policies from "lambda-iam-role.json")
+Event|(set an scheduled event with 5 minutes interval)
+Timeout|10 seconds
+
+By now everything should be up and running. Try to test the function to see if any error is raised. On a successful pass the function should return a "finished" string. Now you can create as many failover spot instances as you want.
+
+## Usage (single instance failover)
+Create 2 launch configurations. One is used as the main servers (probably a spot request), and, in this example, is called **primary-lc**. The second one is used as a failover (you could use an ondemand instance), and is called **failover-lc**. Remember: The name of the launch configurations are not important, we are just using these names for didatic reasons.
+
+Using the **failover-lc** create an Auto Scaling Group (we'll call it **failover-asg**) and make sure you have the following configurations. Do not create any auto scalling events!
+
+Configuration|Value
+-----|------
+Name|(set anything you want)
+Launch Configuration|**failover-lc**
+Min Capacity|0
+Max Capacity|1
+Desired Capacity|0
+Autoscaling|Manual
+
+Next, use the **primary-lc** to create another Auto Scaling Group (we'll call it **primary-asg**). Set the following configurations.
+
+Configuration|Value
+-----|------
+Name|(set anything you want)
+Launch Configuration|**primary-lc**
+Min Capacity|1
+Max Capacity|1
+Desired Capacity|1
+Autoscaling|(anything you want)
+
+In order to tell the lambda script to watch these resources you should set the following tags in the **primary-asg** group.
+
+Key|Value
+-----|------
+_fasi_failover|(name of the **failover-asg** group)
+_fasi_elastic_ip|(reservation-id of the elastic ip to use)
+
+Everything should be up and running! If your instance in the **primary-asg** fails (and it's not replaced) the lambda script automatically launches a new one in the **failover-asg** and change the elastic ip association. As soon as the **primary-asg** succeeds in launching an instance, the **failover-asg** is scaled-down.
+
+## Usage (multiple instances failover)
+Use the same steps as *single instance failover*, but set the *Max Capacity* accordingly. Please note that **elastic ip reassociation does not work with multiple instances**. This means that you must not set the *_fasi_elastic_ip* tag.
---
+++ lambda.py
@@ -0,0 +1,116 @@
+"""Failover AWS Spot Instances."""
+
+import boto3
+
+def main(event, context):
+    client = BotoClientFacade('autoscaling')
+    response = client.multi_request('describe_auto_scaling_groups')
+    groups = {group["AutoScalingGroupName"]: group for group in response["AutoScalingGroups"]}
+
+    mirrors = []
+    for group_name, group in groups.items():
+        for tag in group["Tags"]:
+            if tag["Key"] == "_fasi_failover":
+                failover = tag["Value"]
+                break
+        else:
+            continue
+        mirror = AutoScalingMirror(group_name, failover)
+        mirror.primary_desired_capacity = group["DesiredCapacity"]
+        mirror.primary_instances = group["Instances"]
+        try:
+            mirror.failover_desired_capacity = groups[failover]["DesiredCapacity"]
+            mirror.failover_instances = groups[failover]["Instances"]
+        except KeyError:
+            print "KeyError: group '{}' exists?".format(failover)
+            continue
+        for tag in group["Tags"]:
+            if tag["Key"] == "_fasi_elastic_ip":
+                mirror.elastic_ip = tag["Value"]
+                break
+        mirrors.append(mirror)
+
+    elastic_ips = {}
+    for mirror in mirrors:
+        deficit = mirror.primary_desired_capacity - len(mirror.primary_instances)
+        deficit = deficit if deficit >= 0 else 0
+
+        if deficit <> mirror.failover_desired_capacity:
+            print "Scaling failover to {} instances".format(deficit)
+            client.raw_request('set_desired_capacity', {
+                "AutoScalingGroupName": mirror.failover_name, "DesiredCapacity": deficit,
+                "HonorCooldown": False})
+
+        if mirror.elastic_ip:
+            if len(mirror.primary_instances) == 1:
+                elastic_ips[mirror.elastic_ip] = mirror.primary_instances[0]
+            elif len(mirror.failover_instances) == 1:
+                elastic_ips[mirror.elastic_ip] = mirror.failover_instances[0]
+
+    if elastic_ips:
+        ec2_client = BotoClientFacade('ec2')
+        resp = ec2_client.raw_request('describe_addresses', {"AllocationIds": elastic_ips.keys()})
+
+        for elastic_ip in resp["Addresses"]:
+            current_allocated = elastic_ip.get("InstanceId", None)
+            ip_owner = elastic_ips[elastic_ip["AllocationId"]]["InstanceId"]
+            if ip_owner != current_allocated:
+                print "Associating elastic ip {} to instance {}".format(elastic_ip["AllocationId"],
+                                                                        ip_owner)
+                ec2_client.raw_request('associate_address', {
+                    "InstanceId": ip_owner, "AllocationId": elastic_ip["AllocationId"],
+                    "AllowReassociation": True})
+    return "finished"
+
+class AutoScalingMirror:
+    primary_name = None
+    failover_name = None
+
+    primary_desired_capacity = None
+    failover_desired_capacity = None
+
+    primary_instances = None
+    failover_instances = None
+
+    failover_needed_capacity = None
+    elastic_ip = None
+    elastic_ip_owner = None
+
+    def __init__(self, primary_name, failover_name):
+        self.primary_name = primary_name
+        self.failover_name = failover_name
+
+class BotoClientFacade(object):
+    def __init__(self, service_name):
+        self._boto_client = boto3.client(service_name)
+
+    def multi_request(self, request_name, parameters=None):
+        parameters = {} if parameters is None else parameters
+        if 'NextToken' in parameters:
+            raise Exception("'NextToken' parameter is not allowed in multi_request")
+
+        full_response = {}
+        while True:
+            response = self.raw_request(request_name, parameters)
+            for key, value in response.items():
+                if isinstance(value, list):
+                    if key not in full_response:
+                        full_response[key] = []
+                    full_response[key] += value
+                else:
+                    if key not in full_response:
+                        full_response[key] = []
+                    full_response[key].append(value)
+            try:
+                next_token = response['NextToken']
+                parameters['NextToken'] = next_token
+                if not next_token:
+                    break
+            except KeyError:
+                break
+        return full_response
+
+    def raw_request(self, request_name, parameters=None):
+        parameters = {} if parameters is None else parameters
+        request = getattr(self._boto_client, request_name)
+        return request(**parameters)
